74fbd5e501c15caac044ef26d42934a33c472b4f
Manoj <manoj.wsd@supportgov.in>
2025-07-01 06:55:24 +0530

    s3

--- clients/api.ts
+++ clients/api.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from "graphql-request";
 
 const isClient = typeof window !== 'undefined'
 
-export const  graphqlClient = new GraphQLClient(process.env.NEXT_PUBLIC_API as string, {
+export const  graphqlClient = new GraphQLClient('http://localhost:8000/graphql', {
 
     headers: ()=>{
         const token = isClient? localStorage.getItem('_twitter_token'): ''	
